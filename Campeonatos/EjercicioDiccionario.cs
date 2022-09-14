41c16b1164854db3a84d6b2fe3283d9203f2d2d5
Alan joho <alanjoho55@gmail.com>
2022-09-13 23:03:09 -0300

    Cambios para el ejercicio

---
+++ Campeonatos/EjercicioDiccionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campeonatos
+{
+    public class EjercicioDiccionario
+    {
+        public static void ClubesTitulos()
+        {
+            string equipo = string.Empty;
+            int campeonatos = 0;
+            var titulosXEquipo = new Dictionary<string, int>();
+            Console.WriteLine("Ingrese los equipos del campeonato. ");
+            Console.WriteLine("Y cuando quiera dejar de ingresar coloque la palabra Basta");
+            do
+            {
+                Console.WriteLine("Ingrese un equipo");
+                equipo = Console.ReadLine();
+
+                if (equipo != "Basta")
+                {
+                    Console.WriteLine("Ingrese la cantidad de campeonatos: ");
+                    campeonatos = Convert.ToInt32(Console.ReadLine());
+                    titulosXEquipo.Add(equipo, campeonatos);
+                }
+            }
+            while (equipo != "Basta");
+
+            foreach (KeyValuePair<string, int> titulos in titulosXEquipo.OrderBy(x => x.Value))  //Lambda
+            {
+                Console.WriteLine($"El equipo {titulos.Key} tiene {titulos.Value} titulos ");
+            }
+        }
+    }
+}
--- Campeonatos/Program.cs
+++ Campeonatos/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Campeonatos;
+using static System.Net.Mime.MediaTypeNames;
+
+
+/*En un nuevo Proyecto crear el siguiente programa.
+
+Teniendo una lista de X equipos con X cantidad de titulos, se requiere saber cual es el equipo con mas campeonatos ganados.
+Tener en cuenta que la lista debe ser ingresada por consola a mano, hasta que el usuario indique la palabra "basta".
+Debe mostrarse en en orden con el que tienen mayor cantidad de titulos al que menos tiene.
+
+Una vez terminado y probado el proyecto, se debe crear un nuevo repositorio y subir el proyecto a Git.
+Proyecto: Aplicacion de Consola
+*/
+EjercicioDiccionario.ClubesTitulos();
